31b5daafa295de82a39cc1cbf58c6c4d977df6bf
Matthew Stewart <stewartmatthew6@gmail.com>
2021-11-28 17:21:39 -0500

    Fixed non-aligning files as best as possible

--- fixScripts.py
+++ fixScripts.py
@@ -9,7 +9,7 @@
 # heuristics are needed for this. Of course, this wouldn't be necessary if
 # I understood how the overall file format is structured.
 
-import struct, os
+import sys, struct, os, binascii
 
 def dword(buffer, o):
     return buffer[o+0] | (buffer[o+1] << 8) | (buffer[o+2] << 16) | (buffer[o+3] << 24)
@@ -102,13 +102,22 @@ def copyVoiceLines(scriptName):
     if not jpScriptFile:
         return False
 
-    if len(enScriptFile.scriptList) != len(jpScriptFile.scriptList):
+    if len(enScriptFile.scriptList) != len(jpScriptFile.scriptList) and not scriptName in indexOverride:
         error(scriptName, 'length mismatch (%d, %d)' % (len(jpScriptFile.scriptList), len(enScriptFile.scriptList)))
         return False
 
-    for i in range(len(enScriptFile.scriptList)):
-        enScript = enScriptFile.scriptList[i]
+    for i in range(len(jpScriptFile.scriptList)):
+        j = i
+        if scriptName in indexOverride:
+            if i in indexOverride[scriptName]:
+                j = indexOverride[scriptName][i]
+                if j is None:
+                    continue
+
+        if i >= len(enScriptFile.scriptList):
+            continue;
         jpScript = jpScriptFile.scriptList[i]
+        enScript = enScriptFile.scriptList[j]
 
         voiceStrings = [ b'<voice:', b'<narration:' ]
 
@@ -125,159 +134,41 @@ def copyVoiceLines(scriptName):
     jpScriptFile.save('staging/' + scriptName)
     return True
 
-# List of files with voice lines. Got this with the command:
-#   grep -l '<voice:\|<narration:' dump_directory/* -R
-fileList = [
-'MAP/S_62/S_6298/LASTBATTLE_EV.XSO.Z',
-'MAP/S_62/S_6298/BATTLE_END.XSO.Z',
-'MAP/S_62/S_6298/GOOD_BY.XSO.Z',
-'MAP/S_60/S_6001/DETH_ISLAND.XSO.Z',
-'MAP/S_60/S_6000/DOGICOMA.XSO.Z',
-'MAP/S_60/S_6099/DURAN_EVENT.XSO.Z',
-'MAP/S_60/S_6099/AFTERBATTLE.XSO.Z',
-'MAP/S_61/S_6199/BATTLEEND.XSO.Z',
-'MAP/S_61/S_6199/GARLAND_EV.XSO.Z',
-'MAP/S_11/S_1113/EVENT_2.XSO.Z',
-'MAP/S_11/S_1111/EVENT_ELENA.XSO.Z',
-'MAP/S_11/S_1110/EVENT_2.XSO.Z',
-'MAP/S_11/S_1110/EVENT_1.XSO.Z',
-'MAP/S_40/S_4099/BATTLERIGATY.XSO.Z',
-'MAP/S_40/S_4099/EV_RIGATY.XSO.Z',
-'MAP/S_40/S_4009/DOGI_HARMED.XSO.Z',
-'MAP/S_02/S_0200/SELEND_2.XSO.Z',
-'MAP/S_02/S_0200/GET_BARM.XSO.Z',
-'MAP/S_02/S_0200/HELP_ELENA.XSO.Z',
-'MAP/S_02/S_0200/SEL_YES.XSO.Z',
-'MAP/S_02/S_0200/SEL_PLAY_ARNYA.XSO.Z',
-'MAP/S_02/S_0200/EPILOGUE_1.XSO.Z',
-'MAP/S_02/S_0200/CAMERASCROLL_2.XSO.Z',
-'MAP/S_02/S_0200/GO_HOME_ELENA.XSO.Z',
-'MAP/S_02/S_0200/GO_ELENA_2.XSO.Z',
-'MAP/S_02/S_0200/CAMERASCROLL_1.XSO.Z',
-'MAP/S_02/S_0200/TALKAIDA.XSO.Z',
-'MAP/S_02/S_0200/TALKANTON.XSO.Z',
-'MAP/S_02/S_0200/TALKRICHARD_1.XSO.Z',
-'MAP/S_02/S_0200/SELYES_1.XSO.Z',
-'MAP/S_02/S_0200/EVENT_ALL_BIRM.XSO.Z',
-'MAP/S_02/S_0200/SELNO_3.XSO.Z',
-'MAP/S_02/S_0200/TALKGARDNER.XSO.Z',
-'MAP/S_02/S_0200/SEL_NO.XSO.Z',
-'MAP/S_02/S_0200/SEL_TAKE_BARM.XSO.Z',
-'MAP/S_02/S_0200/TALKHYUGO.XSO.Z',
-'MAP/S_02/S_0200/TALKFIONA.XSO.Z',
-'MAP/S_02/S_0200/TALKDEWY.XSO.Z',
-'MAP/S_02/S_0200/AFTER_BATTLE.XSO.Z',
-'MAP/S_02/S_0200/TALKARNYA.XSO.Z',
-'MAP/S_02/S_0200/SEL_NO2.XSO.Z',
-'MAP/S_02/S_0201/EVENT_1.XSO.Z',
-'MAP/S_10/S_1002/TALK_BOY.XSO.Z',
-'MAP/S_10/S_1000/BYE_DOGI.XSO.Z',
-'MAP/S_10/S_1000/MEET_ELENA.XSO.Z',
-'MAP/S_10/S_1000/TALKGARDNER.XSO.Z',
-'MAP/S_10/S_1004/TALKFRAN_2.XSO.Z',
-'MAP/S_10/S_1004/SELNO_F.XSO.Z',
-'MAP/S_10/S_1004/SELYES_F.XSO.Z',
-'MAP/S_10/S_1004/TALKFLAN.XSO.Z',
-'MAP/S_10/S_1004/HELP_CLIST.XSO.Z',
-'MAP/S_10/S_1001/TALKPORL_2.XSO.Z',
-'MAP/S_10/S_1005/SAID_NO.XSO.Z',
-'MAP/S_10/S_1005/GOODBY_ADOL.XSO.Z',
-'MAP/S_10/S_1005/GO_ISLAND.XSO.Z',
-'MAP/S_10/S_1005/TALKELENA_2.XSO.Z',
-'MAP/S_10/S_1005/TALKELENA.XSO.Z',
-'MAP/S_10/S_1005/TALKDOGI.XSO.Z',
-'MAP/S_10/S_1005/SAID_YES.XSO.Z',
-'MAP/S_10/S_1010/EVENT_TALK_GARDNER.XSO.Z',
-'MAP/S_10/S_1010/BEL_LOOKUP.XSO.Z',
-'MAP/S_33/S_3301/MEET_ELENA.XSO.Z',
-'MAP/S_33/S_3304/LA_QUEBRATA_2.XSO.Z',
-'MAP/S_33/S_3304/LA_QUEBRATA.XSO.Z',
-'MAP/S_33/S_3399/AFTERBATTLECHESTER.XSO.Z',
-'MAP/S_33/S_3399/CHESTER_EVENT.XSO.Z',
-'MAP/S_35/S_3507/EVENT_DULAN.XSO.Z',
-'MAP/S_55/S_5699/EVENT_2.XSO.Z',
-'MAP/S_55/S_5699/BATTLEEND.XSO.Z',
-'MAP/S_55/S_5699/EVENT_1.XSO.Z',
-'MAP/S_55/S_5599/CHESTER_EVENT.XSO.Z',
-'MAP/S_55/S_5599/AFTER_EV.XSO.Z',
-'MAP/S_55/S_5598/MCGUIRE_EVENT.XSO.Z',
-'MAP/S_45/S_4598/DOGI_OVERTHE_ROCK.XSO.Z',
-'MAP/S_45/S_4598/AFTER_BOSS.XSO.Z',
-'MAP/S_45/S_4505/MEET_DOGI.XSO.Z',
-'MAP/S_45/S_4505/GET_BRACELET.XSO.Z',
-'MAP/S_45/S_4505/GET_BRACELET_2.XSO.Z',
-'MAP/S_50/S_5007/TALKALICE.XSO.Z',
-'MAP/S_50/S_5007/TALKCHRIS.XSO.Z',
-'MAP/S_50/S_5007/TALKLIZ.XSO.Z',
-'MAP/S_50/S_5007/FINDFAMILY.XSO.Z',
-'MAP/S_50/S_5107/TALKNICHO.XSO.Z',
-'MAP/S_50/S_5000/EVENT_MEET_ELIZA.XSO.Z',
-'MAP/S_53/S_5304/TALKBOB.XSO.Z',
-'MAP/S_53/S_5304/FINDELENA.XSO.Z',
-'MAP/S_53/S_5304/TALKANDRE.XSO.Z',
-'MAP/S_20/S_2013/TALKEDGAR_1.XSO.Z',
-'MAP/S_20/S_2013/STOPADOL.XSO.Z',
-'MAP/S_20/S_2013/EVENT2.XSO.Z',
-'MAP/S_20/S_2199/DURAN_EVENT.XSO.Z',
-'MAP/S_20/S_2199/ENDBATTLE.XSO.Z',
-'MAP/S_20/S_2009/DEWEYEVENT_1.XSO.Z',
-'MAP/S_20/S_2009/STOPDEWEY_1.XSO.Z',
-'MAP/S_20/S_2009/TALKDEWEY_1.XSO.Z',
-'MAP/S_01/S_0110/TALKADONIS.XSO.Z',
-'MAP/S_01/S_0110/SELNO.XSO.Z',
-'MAP/S_01/S_0110/SELYES.XSO.Z',
-'MAP/S_01/S_0110/CYNTHIA_FIRST.XSO.Z',
-'MAP/S_01/S_0130/AIDA_FIRST.XSO.Z',
-'MAP/S_01/S_0130/RETURNINGPENDANT.XSO.Z',
-'MAP/S_01/S_0180/DYINGDOGI.XSO.Z',
-'MAP/S_01/S_0180/EV_BELHART.XSO.Z',
-'MAP/S_01/S_0150/TALKSISTERNEL.XSO.Z',
-'MAP/S_01/S_0150/TALKPIERRE.XSO.Z',
-'MAP/S_01/S_0150/TALKHALORD.XSO.Z',
-'MAP/S_01/S_0150/RETURNINGPENDANT.XSO.Z',
-'MAP/S_01/S_0150/GET_BARM.XSO.Z',
-'MAP/S_01/S_0150/SEL_YES.XSO.Z',
-'MAP/S_01/S_0150/TALKNICHOLUS_EV.XSO.Z',
-'MAP/S_01/S_0150/TALKAIDA.XSO.Z',
-'MAP/S_01/S_0150/TALKANTON.XSO.Z',
-'MAP/S_01/S_0150/SELYES_1.XSO.Z',
-'MAP/S_01/S_0150/EVENT_NICHOLUS.XSO.Z',
-'MAP/S_01/S_0150/EVENT_ALL_BIRM.XSO.Z',
-'MAP/S_01/S_0150/SELNO_3.XSO.Z',
-'MAP/S_01/S_0150/SEL_NO.XSO.Z',
-'MAP/S_01/S_0150/SEL_TAKE_BARM.XSO.Z',
-'MAP/S_01/S_0150/TALKHYUGO.XSO.Z',
-'MAP/S_01/S_0102/DOGI_EVENT_1.XSO.Z',
-'MAP/S_01/S_0120/AFTER_ISHTARSIVA.XSO.Z',
-'MAP/S_01/S_0120/TALKEDGAR.XSO.Z',
-'MAP/S_01/S_0120/AFTER_ELF.XSO.Z',
-'MAP/S_01/S_0120/TALKABOUTFINAL.XSO.Z',
-'MAP/S_01/S_0120/AFTER_MOUNTAIN.XSO.Z',
-'MAP/S_01/S_0120/AFTER_GARUBA.XSO.Z',
-'MAP/S_01/S_0103/EVENT_2.XSO.Z',
-'MAP/S_01/S_0121/TALKCHESTER.XSO.Z',
-'MAP/S_01/S_0160/TALKROX.XSO.Z',
-'MAP/S_01/S_0140/GO_ELENA.XSO.Z',
-'MAP/S_01/S_0140/TALKABOUTBRO.XSO.Z',
-'MAP/S_01/S_0140/TALKELENA1.XSO.Z',
-'MAP/S_01/S_0100/TALKMARGO.XSO.Z',
-'MAP/S_01/S_0100/RING_OF_JADE.XSO.Z',
-'MAP/S_01/S_0100/TALKJOEL.XSO.Z',
-'MAP/S_01/S_0100/SELNO.XSO.Z',
-'MAP/S_01/S_0100/RING_TO_RANDOLF2.XSO.Z',
-'MAP/S_01/S_0100/TALKBELHERT.XSO.Z',
-'MAP/S_01/S_0100/SELYES.XSO.Z',
-'MAP/S_01/S_0170/TALKHUGO.XSO.Z',
-'MAP/S_01/S_0170/GET_BARM.XSO.Z',
-'MAP/S_01/S_0170/EVENT_ALL_BIRM.XSO.Z',
-'MAP/S_01/S_0170/SELNO_3.XSO.Z',
-'MAP/S_01/S_0170/SEL_TAKE_BARM.XSO.Z',
-'MAP/S_25/S_2503/ADOLAWAKEN.XSO.Z',
-]
+def analyzeScript(scriptFile, outname=None):
+    scriptFile = parseScriptFile(scriptFile)
+    if not scriptFile:
+        return False
 
-successCount = 0
-for i in range(len(fileList)):
-    if copyVoiceLines(fileList[i]):
-        successCount += 1
+    if outname is None:
+        f = sys.stdout
+    else:
+        f = open(outname, 'w')
+    for i in range(len(scriptFile.scriptList)):
+        f.write('SCRIPT ' + hex(i) + ':\n')
+        try:
+            f.write(scriptFile.scriptList[i].decode('shift-jis'))
+        except:
+            f.write('ERROR DECODING STRING: ' + binascii.hexlify(scriptFile.scriptList[i]).decode())
+        f.write('\n\n')
+    if not outname is None:
+        f.close()
 
-print('Successfully converted ' + str(successCount) + ' / ' + str(len(fileList)) + ' files')
+def analyzeScriptPair(scriptName):
+    analyzeScript('dump_us_2020/' + scriptName, 'analyze_en')
+    analyzeScript('dump_main_2020/' + scriptName, 'analyze_jp')
+
+
+from scriptInfo import *
+
+
+def convertAllVoiceLines():
+    successCount = 0
+    for i in range(len(fileList)):
+        if copyVoiceLines(fileList[i]):
+            successCount += 1
+    print('Successfully converted ' + str(successCount) + ' / ' + str(len(fileList)) + ' files')
+
+convertAllVoiceLines()
+
+#analyzeScript('staging/MAP/S_62/S_6298/GOOD_BY.XSO.Z')
+#analyzeScriptPair('MAP/S_01/S_0170/TALKHUGO.XSO.Z')
